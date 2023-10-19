62cdb3c77083344c6607b7c4bf50f94213dd4418
“Jake” <zoulux@126.com>
2023-10-19 13:35:33 +0800

    feat: collect

--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"io"
-	"net/http"
 	"regexp"
 
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
+
+	"github.com/zoulux/crawler-go/collect"
 )
 
 var headerRe = regexp.MustCompile(`<div class="news_li"[\s\S]*?<h2>[\s\S]*?<a.*?target="_blank">([\s\S]*?)</a>`)
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
-
+	url := "https://book.douban.com/subject/1007305/"
+	var f collect.Fetcher = collect.BaseFetch{}
+	body, err := f.Get(url)
 	if err != nil {
 		fmt.Println("read content failed:%v", err)
 		return
@@ -37,36 +32,4 @@ func main() {
 		fmt.Printf("Review %d: %s \n", i, title)
 	})
 
-}
-
-func Fetch(url string) ([]byte, error) {
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%d", resp.StatusCode)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DeterminEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return io.ReadAll(utf8Reader)
-}
-
-func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
-
-	bytes, err := r.Peek(1024)
-
-	if err != nil {
-		fmt.Println("fetch error:%v", err)
-		return unicode.UTF8
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
